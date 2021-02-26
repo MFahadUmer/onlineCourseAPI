2473c18d8be2ada54fd7a5ebe988e00b7db69ec6
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2021-02-26 15:02:51 +0500

    Adds Users Api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def index
+    render json: User.all
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201
+    else
+      render json: user.errors,status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :user_type, :password)
+  end
 end
