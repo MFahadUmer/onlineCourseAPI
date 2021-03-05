757accf55ee37d0205ff8c1954a0d940f38d7fcb
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2021-03-05 19:23:04 +0500

    Remove Rubocop Error

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create(id: 1, first_name: 'Muhammad', last_name: 'Fahad Umer', username: 'user', user_type: 'user', email: 'eng.fahadumer1@gmail.com', password:'asdfgh') }
-  let(:user2) { User.create(id: 2, first_name: 'Muhammad', last_name: 'Farooq Ahmed', username: 'author', user_type: 'author', email: 'eng.fahadumer2@gmail.com', password:'asdfgh') }
-  let(:course) { user2.courses.build(id: 1, course_title: 'React and Redux', course_details: 'Its a very nice course', course_image: 'https://www.wrappixel.com/wp-content/uploads/2019/08/guide-for-react-redux.jpg', course_price: '100', course_requirements: 'No Requirements', course_difficulty: 'Beginners') }
-  let(:favourite) { user.favourites.build(id: 1, course_id: 1)}
+  let(:user) do
+    User.create(id: 1, first_name: 'Muhammad', last_name: 'Fahad Umer', username: 'user', user_type: 'user',
+                email: 'eng.fahadumer1@gmail.com', password: 'asdfgh')
+  end
+  let(:user2) do
+    User.create(id: 2,
+                first_name: 'Muhammad', last_name: 'Farooq Ahmed', username: 'author', user_type: 'author',
+                email: 'eng.fahadumer2@gmail.com', password: 'asdfgh')
+  end
+  let(:course) do
+    user2.courses.build(id: 1,
+                        course_title: 'React and Redux', course_details: 'Its a very nice course',
+                        course_image: 'https://www.wrappixel.com/wp-content/uploads/2019/08/guide-for-react-redux.jpg',
+                        course_price: '100', course_requirements: 'No Requirements', course_difficulty: 'Beginners')
+  end
+  let(:favourite) { user.favourites.build(id: 1, course_id: 1) }
   it 'should be invalid if firstname is less than 4 characters' do
     user.first_name = '123'
     expect(user).to_not be_valid
